ab609ea4d7254e4e753b2eaebdc762784838abbe
awesomedana <dana042118@gmail.com>
2021-03-04 19:00:41 +0900

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// OBJECT DESTRUCTURING
+
 const person = {
   name: 'Hayeon',
   age: 30,
@@ -18,8 +20,6 @@ if (city && temperature) {
   console.log(`It's ${temperature} in ${city}.`);
 }
 
-console.log('-------------------------------------------');
-
 const book = {
   title: 'Ego is the Enemy',
   author: 'Ryan Holiday',
@@ -31,3 +31,15 @@ const book = {
 const { name: publisherName = 'Self-Published' } = book.publisher;
 
 console.log(publisherName);
+console.log('----------------------------------------');
+
+// ARRAY DESTRUCTURING
+
+const address = ['Awesome Street', 'Minato', 'Tokyo', '12345'];
+const [, , state = 'New York'] = address;
+console.log(`You are in ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$3.00'];
+const [itemName, smallPrice, mediumPrice, largePrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
+
