5bfaf2ca8e52041ee36788decdf0b02d43a330bd
awesomedana <dana042118@gmail.com>
2021-03-09 20:23:14 +0900

    Sorting Redux Data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -134,6 +134,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
   });
 };
 
@@ -160,12 +166,12 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 2,
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 // console.log('setTextFilter');
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 
 // console.log('sortByDate');
 // store.dispatch(sortByDate());
 // console.log('sortByAmount');
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 
 // console.log('setStartDate');
 // store.dispatch(setStartDate(125));
